5f452f3d3bd806995ff4df0e410d73554dbb5bec
Tim James <timbjames@gmail.com>
2013-02-08 21:38:39 +0000

    updated flickr feed to use my id

--- js/script.js
+++ js/script.js
@@ -19,16 +19,18 @@ $(function ($) {
     });
 
     /* get flickr feed */
-    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-      {
-          tags: "mount rainier",
-          tagmode: "any",
-          format: "json"
-      },
+    /* 84100512@N02 */  
+    //$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+    //{
+    //      tags: "mount rainier",
+    //      tagmode: "any",
+    //      format: "json"
+    //  },
+    $.getJson("http://api.flickr.com/services/feeds/groups_pool.gne?id=84100512@N02&lang=en-us&format=json&jsoncallback=?",      
       function (data) {
           var $li, $a, $img;
           $.each(data.items, function (i, item) {
-              $li = $('<li />').append($('<a />', { attr: { href: '#'} }).append($('<img />', { attr: { src: item.media.m} })));
+              $li = $('<li />').append($('<a />', { attr: { href: item.link } }).append($('<img />', { attr: { alt: item.title, src: item.media.m.replace("_m.jpg", "_s.jpg") } })));
               $li.appendTo("#flickr-feed");
               //<li><a href="" title=""><img alt="Flickr" src="/img/6795452380_3ffa150da3_s.jpg"></a></li>
               //$("<img/>").attr("src", item.media.m).appendTo("#flickr-feed");
